879c608d714ff097b5e481a6ebbd2d2a6b497dbc
Korsun Pavel <45067434+kcvpll@users.noreply.github.com>
2021-03-02 13:55:45 +0300

    Added Skip Button

--- app/src/main/java/com/example/cityguide/Common/OnBoarding.java
+++ app/src/main/java/com/example/cityguide/Common/OnBoarding.java
@@ -3,13 +3,20 @@ package com.example.cityguide.Common;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Html;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.cityguide.HelperClasses.SliderAdapter;
 import com.example.cityguide.R;
+import com.example.cityguide.User.UserDashboard;
 
 public class OnBoarding extends AppCompatActivity {
 
@@ -17,14 +24,19 @@ public class OnBoarding extends AppCompatActivity {
     LinearLayout dotsLayout;
     SliderAdapter sliderAdapter;
     TextView[] dots;
+    Button letsGetStarted;
+    Animation animation;
+    int currentPos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_on_boarding);
 
         viewPager = findViewById(R.id.slider);
         dotsLayout = findViewById(R.id.dots);
+        letsGetStarted = findViewById(R.id.get_started_btn);
 
 
         sliderAdapter = new SliderAdapter(this);
@@ -35,6 +47,20 @@ public class OnBoarding extends AppCompatActivity {
 
 
     }
+
+
+    public void skip(View view){
+        startActivity(new Intent(getApplicationContext(), UserDashboard.class));
+
+    }
+
+    public void next(View view){
+        viewPager.setCurrentItem(currentPos+1);
+
+
+    }
+
+
 
     private void addDots(int position){
 
@@ -64,8 +90,23 @@ public class OnBoarding extends AppCompatActivity {
 
         @Override
         public void onPageSelected(int position) {
-
             addDots(position);
+            currentPos = position;
+
+
+
+            if (position == 0){
+                letsGetStarted.setVisibility(View.INVISIBLE);
+            }else if (position == 1){
+                letsGetStarted.setVisibility(View.INVISIBLE);
+            }else if (position == 2){
+                letsGetStarted.setVisibility(View.INVISIBLE);
+            }else {
+                animation = AnimationUtils.loadAnimation(OnBoarding.this,R.anim.bottom_anim);
+                letsGetStarted.setAnimation(animation);
+                letsGetStarted.setVisibility(View.VISIBLE);
+            }
+
 
         }
 
