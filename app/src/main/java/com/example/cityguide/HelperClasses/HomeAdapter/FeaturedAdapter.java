691c752b0a1deba39108d5b532e301f1af505d4a
Korsun Pavel <45067434+kcvpll@users.noreply.github.com>
2021-03-07 03:25:53 +0300

    Adding featured locations

---
+++ app/src/main/java/com/example/cityguide/HelperClasses/HomeAdapter/FeaturedAdapter.java
@@ -0,0 +1,72 @@
+package com.example.cityguide.HelperClasses.HomeAdapter;
+
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.cityguide.R;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class FeaturedAdapter extends RecyclerView.Adapter <FeaturedAdapter.FeaturedViewerHolder> {
+
+    ArrayList<FeaturedHelperClass> featuredLocations;
+
+    public FeaturedAdapter(ArrayList<FeaturedHelperClass> featuredLocations) {
+        this.featuredLocations = featuredLocations;
+    }
+
+    @NonNull
+    @Override
+    public FeaturedViewerHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.featured_card_design, parent,false);
+        FeaturedViewerHolder featuredViewerHolder = new FeaturedViewerHolder(view);
+        return featuredViewerHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FeaturedViewerHolder holder, int position) {
+
+        FeaturedHelperClass featuredHelperClass = featuredLocations.get(position);
+
+        holder.image.setImageResource(featuredHelperClass.getImage());
+        holder.title.setText(featuredHelperClass.getTitle());
+        holder.desc.setText(featuredHelperClass.getDescription());
+
+    }
+
+    @Override
+    public int getItemCount() {
+
+        return featuredLocations.size();
+    }
+
+
+    public static class FeaturedViewerHolder extends RecyclerView.ViewHolder{
+
+        ImageView image;
+        TextView title, desc;
+
+
+        public FeaturedViewerHolder(@NonNull View itemView) {
+            super(itemView);
+
+            image = itemView.findViewById(R.id.featured_image);
+            title = itemView.findViewById(R.id.featured_title);
+            desc = itemView.findViewById(R.id.featured_description);
+
+
+        }
+    }
+
+
+
+}
--- app/src/main/java/com/example/cityguide/User/UserDashboard.java
+++ app/src/main/java/com/example/cityguide/User/UserDashboard.java
@@ -1,18 +1,53 @@
 package com.example.cityguide.User;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.widget.LinearLayout;
 
+import com.example.cityguide.HelperClasses.HomeAdapter.FeaturedAdapter;
+import com.example.cityguide.HelperClasses.HomeAdapter.FeaturedHelperClass;
 import com.example.cityguide.R;
 
+import java.util.ArrayList;
+
 public class UserDashboard extends AppCompatActivity {
+
+    RecyclerView featuredRecycler;
+    RecyclerView.Adapter adapter;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_user_dashboard);
+
+        featuredRecycler = findViewById(R.id.featured_recycler);
+        
+        featuredRecycler();
+
+
+    }
+
+    private void featuredRecycler() {
+        featuredRecycler.setHasFixedSize(true);
+        featuredRecycler.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+
+
+        ArrayList<FeaturedHelperClass> featuredLocations = new ArrayList<>();
+
+        featuredLocations.add(new FeaturedHelperClass(R.drawable.kfc, "KFC", "Это просто текст бла бла бла"));
+        featuredLocations.add(new FeaturedHelperClass(R.drawable.kfc, "Bakers", "Это просто текст бла бла бла"));
+        featuredLocations.add(new FeaturedHelperClass(R.drawable.kfc, "Zara", "Это просто текст бла бла бла"));
+
+        adapter = new FeaturedAdapter(featuredLocations);
+        featuredRecycler.setAdapter(adapter);
+
+
     }
 }
