9c4d44ff565e1d0d43b403884b9b3223e2948d9e
Korsun Pavel <45067434+kcvpll@users.noreply.github.com>
2021-03-18 18:24:22 +0300

    Login Screen for b2b

--- app/src/main/java/com/example/cityguide/Common/LoginSingup/RetailerStartUpScreen.java
+++ app/src/main/java/com/example/cityguide/Common/LoginSingup/RetailerStartUpScreen.java
@@ -1,9 +1,13 @@
 package com.example.cityguide.Common.LoginSingup;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Pair;
+import android.view.View;
 import android.view.WindowManager;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.cityguide.R;
 
@@ -15,4 +19,22 @@ public class RetailerStartUpScreen extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_retailer_start_up_screen);
     }
+
+
+    public void callLoginScreen(View view) {
+
+        Intent intent = new Intent(getApplicationContext(), Login.class);
+
+        Pair[] pairs = new Pair[1];
+        pairs[0] = new Pair<View, String>(findViewById(R.id.login_btn), "transition_login");
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(RetailerStartUpScreen.this, pairs);
+            startActivity(intent, options.toBundle());
+        } else {
+            startActivity(intent);
+        }
+
+    }
+
 }
