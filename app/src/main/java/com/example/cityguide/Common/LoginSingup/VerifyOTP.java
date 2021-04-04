b0038fa65b1f0528ec92b38c1bd21e8e437a2104
Korsun Pavel <45067434+kcvpll@users.noreply.github.com>
2021-04-04 15:20:47 +0300

    Verify Number Screen

---
+++ app/src/main/java/com/example/cityguide/Common/LoginSingup/VerifyOTP.java
@@ -0,0 +1,16 @@
+package com.example.cityguide.Common.LoginSingup;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.cityguide.R;
+
+public class VerifyOTP extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify_otp);
+    }
+}
