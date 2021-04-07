6fc1b5a62f761071a4b126bdc9dc8103068069c4
Korsun Pavel <45067434+kcvpll@users.noreply.github.com>
2021-04-07 17:12:30 +0300

    Detail resert options

---
+++ app/src/main/java/com/example/cityguide/Common/LoginSingup/ForgetPasswordSuccessMessage.java
@@ -0,0 +1,16 @@
+package com.example.cityguide.Common.LoginSingup;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.cityguide.R;
+
+public class ForgetPasswordSuccessMessage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget_success_message);
+    }
+}
---
+++ app/src/main/java/com/example/cityguide/Common/LoginSingup/SetNewPassword.java
@@ -0,0 +1,16 @@
+package com.example.cityguide.Common.LoginSingup;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.cityguide.R;
+
+public class SetNewPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_new_password);
+    }
+}
