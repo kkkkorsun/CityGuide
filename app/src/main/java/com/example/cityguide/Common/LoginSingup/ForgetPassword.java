3b7f72695aca02fdd0396ca70ab54f309547b646
Korsun Pavel <45067434+kcvpll@users.noreply.github.com>
2021-04-07 14:18:13 +0300

    Forget password screen

---
+++ app/src/main/java/com/example/cityguide/Common/LoginSingup/ForgetPassword.java
@@ -0,0 +1,16 @@
+package com.example.cityguide.Common.LoginSingup;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.cityguide.R;
+
+public class ForgetPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget_password);
+    }
+}
