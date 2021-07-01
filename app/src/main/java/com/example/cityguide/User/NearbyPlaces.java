9826c2bacd643a43fc5f9b80d189dfb910c83c23
Korsun Pavel <45067434+kcvpll@users.noreply.github.com>
2021-07-01 16:09:42 +0300

    Update GoogleMaps

--- app/src/main/java/com/example/cityguide/User/NearbyPlaces.java
+++ app/src/main/java/com/example/cityguide/User/NearbyPlaces.java
@@ -3,18 +3,32 @@ package com.example.cityguide.User;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.cityguide.R;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.PermissionDeniedResponse;
@@ -22,10 +36,14 @@ import com.karumi.dexter.listener.PermissionGrantedResponse;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.single.PermissionListener;
 
-public class NearbyPlaces extends AppCompatActivity implements OnMapReadyCallback {
+public class NearbyPlaces extends AppCompatActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
 
     boolean isPermissionGranted;
     GoogleMap mGoogleMap;
+    ImageView backBtn;
+    FloatingActionButton btnLoc;
+    private FusedLocationProviderClient mLocationClient;
+    private int GPS_REQUES_CODE = 9001;
 
 
     @Override
@@ -34,15 +52,86 @@ public class NearbyPlaces extends AppCompatActivity implements OnMapReadyCallbac
         setContentView(R.layout.activity_nearby_places);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-
+        btnLoc = findViewById(R.id.btn_location);
 
         checkMyPermission();
 
+        initMap();
+
+        backBtn = findViewById(R.id.back_pressed);
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NearbyPlaces.super.onBackPressed();
+            }
+        });
+
+
+        mLocationClient = new FusedLocationProviderClient(this);
+
+        btnLoc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getCurrLoc();
+            }
+        });
+
+    }
+
+    private void initMap() {
         if (isPermissionGranted){
-            SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.fragment);
-            supportMapFragment.getMapAsync(this);
+            if (isGPSenable()){
+                SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.fragment);
+                supportMapFragment.getMapAsync(this);
+            }
 
         }
+    }
+
+    private boolean isGPSenable (){
+
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        boolean providerEnable = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        if (providerEnable){
+            return true;
+        }else {
+
+            AlertDialog alertDialog = new AlertDialog.Builder(this)
+                    .setTitle("GPS Permission")
+                    .setMessage("GPS is required for this app to work. Please enable GPS")
+                    .setPositiveButton("Yes",((dialog, which) -> {
+                        Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivityForResult(intent, GPS_REQUES_CODE);
+                    }))
+                    .setCancelable(false)
+                    .show();
+        }
+        return false;
+    }
+
+    @SuppressLint("MissingPermission")
+    private void getCurrLoc() {
+
+        mLocationClient.getLastLocation().addOnCompleteListener(task -> {
+            if (task.isSuccessful()){
+                Location location = task.getResult();
+                gotoLocation(location.getLatitude(), location.getLongitude());
+            }
+        });
+
+    }
+
+    private void gotoLocation(double latitude, double longitude) {
+
+        LatLng LatLng = new com.google.android.gms.maps.model.LatLng(latitude,longitude);
+
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(LatLng, 18);
+        mGoogleMap.moveCamera(cameraUpdate);
+        mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
     }
 
     private void checkMyPermission() {
@@ -81,5 +170,35 @@ public class NearbyPlaces extends AppCompatActivity implements OnMapReadyCallbac
 
     }
 
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
 
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GPS_REQUES_CODE){
+            LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+            boolean providerEnable = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+            if (providerEnable){
+                Toast.makeText(this, "GPS is enable", Toast.LENGTH_SHORT).show();
+            }else {
+                Toast.makeText(this, "GPS is not enable", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 }
